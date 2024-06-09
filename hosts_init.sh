fcd61822c52301064ee4f84326a750bcb2d4d182
joederfmoura <joederfmoura@gmail.com>
2024-06-09 02:00:55 -0400

    Ajustes provsion.sh e hosts.init.sh
    Ajustes provsion.sh e hosts.init.sh

--- hosts_init.sh
+++ hosts_init.sh
@@ -2,8 +2,6 @@
 
 #Copiando chave publica SSH
 mkdir -p /home/vagrant/.ssh
-#cp /vagrant/key/id_rsa.pub /home/vagrant/.ssh/id_rsa.pub
-#chmod 400 /home/vagrant/.ssh/id_rsa.pub
 
 # Garantindo a chave publica
 AUTH_KEY=$(head -n1 /home/vagrant/.ssh/authorized_keys)
